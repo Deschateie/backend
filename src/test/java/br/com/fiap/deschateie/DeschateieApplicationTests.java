e3a26cd1b69b14eeccba4c09d1f5881561b9fcfa
Marco A <mabo.marcoantonio@gmail.com>
2019-09-03 18:16:00 -0300

    Fix Test Unit

--- src/test/java/br/com/fiap/deschateie/DeschateieApplicationTests.java
+++ src/test/java/br/com/fiap/deschateie/DeschateieApplicationTests.java
@@ -1,4 +1,6 @@
-package br.com.fiap.DeschateieDemo;
+package br.com.fiap.deschateie;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +9,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DeschateieDemoApplicationTests {
+public class DeschateieApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Long n1 = new Long(0);
+		Long n2 = new Long(0);
+		assertEquals(n1, n2);
 	}
 
 }
